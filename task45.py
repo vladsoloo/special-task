baf202b7ffd2a89c2b97ce668a34775343b8fbe9
Xlxlrdyyy <solo.vlad55555@icloud.com>
2024-12-10 11:23:46 +0000

    сделал

---
+++ task45.py
@@ -0,0 +1,12 @@
+num1 = int(input("Введите num1: "))
+num2 = int(input("Введите num2: "))
+
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = round(num1 / num2, 2)
+print("Сумма равно: ", sum)
+print("Разность равна: ", difference)
+print("Произвнедение равно: ", product)
+print("Частное равно: ", quotient)
