cd61bca3e4df84e255b230866e55d822616ae07d
Xlxlrdyyy <solo.vlad55555@icloud.com>
2024-12-10 08:41:44 +0000

    сделал

---
+++ task32.py
@@ -0,0 +1,4 @@
+residents = int(input("Введите количество жителей страны: "))
+square = int(input("Введите площадь страны: "))
+population_density = residents / square
+print("Плотность населения: ", population_density)
