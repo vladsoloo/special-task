f43c628a0c16431461c855815df45d224072d6e2
Xlxlrdyyy <solo.vlad55555@icloud.com>
2024-12-10 12:00:05 +0000

    сделал

---
+++ task21.py
@@ -0,0 +1,29 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+
+# а)
+a = 5.8
+print(a)
+
+b = -7.9
+print(b)
+
+b = a
+print(b)
+
+a = b
+print(a)
+
+# б)
+
+a = 0
+print(a)
+
+b = -9.99
+print(b)
+
+b = a
+print(b)
+
+a = b
+print(a)
