b4f4791e72f06181ffa4807beed64120eaa26e83
Xlxlrdyyy <solo.vlad55555@icloud.com>
2024-12-12 10:23:16 +0000

    сделал

---
+++ task54.py
@@ -0,0 +1,10 @@
+x = int(input("Введите возраст Тани: "))
+y = int(input("Введите возраст Мити: "))
+
+average_age = (x + y) / 2
+
+difference_tanya = x - average_age
+difference_mitya = x - average_age
+
+print(f"Средний возраст: {average_age} лет."
+print()
